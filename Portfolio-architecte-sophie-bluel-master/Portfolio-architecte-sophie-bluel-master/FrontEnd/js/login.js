dab2f896012f9a64f3b745e2c832879443f2570c
aymeric-bascunana <130646537+aymeric-bascunana@users.noreply.github.com>
2023-08-15 17:51:28 +0200

    modification du 15/08/2023

--- Portfolio-architecte-sophie-bluel-master/Portfolio-architecte-sophie-bluel-master/FrontEnd/js/login.js
+++ Portfolio-architecte-sophie-bluel-master/Portfolio-architecte-sophie-bluel-master/FrontEnd/js/login.js
@@ -1,12 +1,39 @@
-function validateForm() {
-  var email = document.getElementById("E-mail").value;
-  var password = document.getElementById("motDePasse").value;
+fetch("http://localhost:5678/api/users/login")
+  .then((data) => data.json())
+  .then((login) => {
+    console.log(login);
+    createLogin(login);
+  });
 
-  // Valider l'email et password ou echec
-  if (email === "utilisateur@example.com" && password === "motdepasse") {
-    document.getElementById("messageErreur").textContent = "Connexion réussie!";
-  } else {
-    document.getElementById("messageErreur").textContent =
-      "Adresse e-mail ou mot de passe incorrect. Veuillez réessayer.";
-  }
+function validateForm() {
+  const email = document.getElementById("E-mail").value;
+  const password = document.getElementById("motDePasse").value;
+
+  const formData = {
+    E_mail: email,
+    motDePasse: password,
+  };
+
+  const url = "http://localhost:5678/api/users/login";
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur lors de la requête");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      createLogin(data);
+    })
+    .catch((error) => {
+      console.error("Erreur:", error);
+    });
 }
